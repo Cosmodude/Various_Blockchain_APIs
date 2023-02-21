9e0281bb74b5b06b6a3b5d5fb70c9980c8913a4c
Hackdady <lenskii.vm19@physics.msu.ru>
2023-02-21 13:57:27 +0900

    changed CMC script name

--- CMC/Token_price_API_script.py
+++ CMC/Token_price_API_script.py
@@ -1,19 +1,21 @@
 from dotenv import load_dotenv
 load_dotenv()
 import os
+from web3 import Web3
 import requests
 axie_url = "https://api.opensea.io/api/v1/collection/axie"
 ice_poker_url ="https://api.opensea.io/api/v1/collection/decentral-games-ice"
 coinmarketcap_url ="https://pro-api.coinmarketcap.com/v2/tools/price-conversion"
 
 
-def CoinMarketCap_API(url,symbol):
+def CoinMarketCap_API(symbol,currency):
+    coinmarketcap_url ="https://pro-api.coinmarketcap.com/v2/tools/price-conversion"
     headers = {
         'Accepts': 'application/json',
         'X-CMC_PRO_API_KEY': os.getenv('X-CMC_PRO_API_KEY') ,
             }
-    parameters = { "amount": 1, "symbol": symbol, "convert" : "USD"}
-    json= requests.get(url,params=parameters, headers=headers).json()
+    parameters = { "amount": 1, "symbol": symbol, "convert" : currency}
+    json= requests.get(coinmarketcap_url,params=parameters, headers=headers).json()
     return json
 
 def OpenSea_API(url):
@@ -25,7 +27,7 @@ print(axie_json["collection"]["stats"]["floor_price"])
 print(axie_json["collection"]["primary_asset_contracts"][0]["address"])'''
 
 Ice_poker_json=OpenSea_API(ice_poker_url)
-print(CoinMarketCap_API(coinmarketcap_url, "ICE")["data"][0]["quote"]["USD"]["price"])
+print(CoinMarketCap_API("ETH","SOL")["data"][0]["quote"]["SOL"]["price"])
 """BTC_json= CoinMarketCap_API(coinmarketcap_url, "ICE")
 
 print(BTC_json)
