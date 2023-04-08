64c8d2bae390b04e6286db87e26c71612a30e191
Hackdady <lenskii.vm19@physics.msu.ru>
2023-04-08 09:45:00 +0900

    new API script, requers subscription

---
+++ Crypto_APIs/Token_transfers_by_tx.py
@@ -0,0 +1,30 @@
+import http.client
+import os
+from dotenv import load_dotenv
+from web3 import Web3
+load_dotenv()
+
+api_key= os.environ.get('CryptoAPIs_API_KEY')
+
+connection = http.client.HTTPConnection("https://rest.cryptoapis.io")
+querystring = {"context":"SampleTX","limit":50,"offset":0}
+headers = {
+  'Content-Type': "application/json",
+  'X-API-Key': api_key
+}
+
+connection.request("GET",
+"blockchain-data,\
+ethereum,\
+mainnet,\
+transactions,\
+0xab9875572bd672c02a2d4ee00f6d9e20d3d4fffbc04e71ee72901ce8adf392f0,\
+tokens-transfers",
+headers=headers, 
+params=querystring 
+)
+
+result = conn.getresponse()
+data = result.read()
+
+print(data.decode("utf-8"))
